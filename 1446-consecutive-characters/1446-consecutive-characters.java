ae49900fd71ffea34e5669e9fb6a3d44ece9c38d
Hariharan <hariharank0820@gmail.com>
2026-07-02 05:19:14 +0530

    Time: 85 ms (2.3%), Space: 43.1 MB (90.51%) - LeetHub

---
+++ 1446-consecutive-characters/1446-consecutive-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxPower(String s) {
+        int cnt=1,max=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                cnt++;
+                max = Math.max(max,cnt);
+            }else{
+                cnt=1;
+            }
+        }
+        return max;
+    }
+}
